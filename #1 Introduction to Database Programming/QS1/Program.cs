e566d936fbec747c34144b3f0da62b8f5a97dfa0
Naphat-Sua <naphat.sua@dome.tu.ac.th>
2024-08-28 11:03:50 +0700

    Add files via upload

---
+++ #1 Introduction to Database Programming/QS1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        int N1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        int N2 = Convert.ToInt32(Console.ReadLine());
+
+        int Sum = N1 + N2;
+        int Difference = N1 - N2;
+        int Product = N1 * N2;
+        double Quotient = (double)N1 / N2;
+
+        Console.WriteLine("Sum: " + Sum);
+        Console.WriteLine("Difference: " + Difference);
+        Console.WriteLine("Product: " + Product);
+        Console.WriteLine("Quotient: " + Quotient);
+    }
+}
---
+++ #1 Introduction to Database Programming/QS2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive integer: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= number)
+        {
+            sum += i;
+            i++;
+        }
+
+        Console.WriteLine($"The sum of numbers from 1 to {number} is: {sum}");
+    }
+}
